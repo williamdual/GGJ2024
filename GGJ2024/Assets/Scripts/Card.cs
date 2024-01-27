3618d9a0f6b149ff020c3b4ea642744ae04d2a55
Vitaly Dufour <vitdufour@gmail.com>
2024-01-27 12:13:16 -0500

    Initital Project Structure

---
+++ GGJ2024/Assets/Scripts/Card.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject {
+    public string cardName;
+    public string description;
+    public Sprite artwork;
+
+    public int energyCost;
+
+    
+
+}
